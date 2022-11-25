efd833cf5f3e394ed2a96159ddc6045bc58570ed
vasily-mishanin <vasilymishanin@gmail.com>
2022-11-25 11:36:05 +0300

    fix: add DB connection and fix start script

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,19 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    //mongodb+srv://<username>:<password>@todocluster.kf4x7.mongodb.net/?retryWrites=true&w=majority
+    await mongoose.connect(
+      'mongodb+srv://vasily:q5wvsFfhKBiB0rsf@todocluster.kf4x7.mongodb.net/managementApp?retryWrites=true&w=majority'
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
