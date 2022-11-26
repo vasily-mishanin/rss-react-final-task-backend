a79f870c3a9b3ffc092f5bc520a81186ded28649
vasily-mishanin <vasilymishanin@gmail.com>
2022-11-26 19:22:22 +0300

    feat: add env vars

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 import { PORT } from './constants';
 
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+// console.log(process.env);
+
 import * as serverService from './services/server.service';
 
 (async () => {
   try {
     //mongodb+srv://<username>:<password>@todocluster.kf4x7.mongodb.net/?retryWrites=true&w=majority
     await mongoose.connect(
-      'mongodb+srv://vasily:q5wvsFfhKBiB0rsf@todocluster.kf4x7.mongodb.net/managementApp?retryWrites=true&w=majority'
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todocluster.kf4x7.mongodb.net/managementApp?retryWrites=true&w=majority`
     );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
